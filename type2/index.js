4c5724d11996800e886f34e85c47b54e12cfcc40
Niks <ktm.softwear@gmail.com>
2025-06-07 22:13:31 +0300

    First commit

---
+++ type2/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const fullscreenMenu = document.querySelector(".fullscreen-menu");
+
+  // Показываем меню сразу при загрузке
+  fullscreenMenu.classList.add("active");
+
+  // Закрытие меню при клике на пункт
+  const menuItems = document.querySelectorAll(".menu-item");
+  menuItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      fullscreenMenu.classList.remove("active");
+    });
+  });
+});
